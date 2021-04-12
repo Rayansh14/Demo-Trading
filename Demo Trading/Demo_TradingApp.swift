5112dd0cbabdaf195114fdc461eb3a4b7ef0761f
Rayansh14 <rayanshgupta1406@gmail.com>
2021-04-12 15:31:07 +0530

    Initial Commit

---
+++ Demo Trading/Demo_TradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_TradingApp.swift
+//  Demo Trading
+//
+//  Created by Rayansh Gupta on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Demo_TradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
